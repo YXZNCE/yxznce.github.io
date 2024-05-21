96164b255f7a9871b55a98e5ee5ea436b475d390
Yen Lamire <96825536+YenLamire@users.noreply.github.com>
2024-05-21 10:10:31 +0800

    Add files via upload

---
+++ Urban Thrifts/scripts/index.js
@@ -0,0 +1,13 @@
+
+
+function gotoMap(){
+    window.location.href = "https://maps.app.goo.gl/JeYFPvDR41Ls8k359";
+}
+
+function dlwin(){
+    window.location.href = "./files/Release.zip";
+}
+
+function dland(){
+    window.location.href = "./files/UrbanThrift.apk";
+}
